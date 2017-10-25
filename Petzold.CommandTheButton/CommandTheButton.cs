ab72a1e579ff0bc4313c8867d34ee4195733ea21
Budya <budinskiy_d@1mpf.lan>
2017-10-25 16:53:29 +0300

    done 25/10/17

---
+++ Petzold.CommandTheButton/CommandTheButton.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Petzold.CommandTheButton
+{
+    class CommandTheButton : Window
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Application app = new Application();
+            app.Run(new CommandTheButton());
+
+        }
+
+        public CommandTheButton()
+        {
+            Title = "Command The Button";
+            Button btn = new Button();
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            btn.VerticalAlignment = VerticalAlignment.Center;
+            btn.Command = ApplicationCommands.Paste;
+            btn.Content = ApplicationCommands.Paste.Text;
+            Content = btn;
+
+
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Paste,
+                PasteOnExecute, PasteCanExecute));
+            
+        }
+
+        private void PasteCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = Clipboard.ContainsText();
+
+        }
+
+        private void PasteOnExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            Title = Clipboard.GetText();
+
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+            Title = "Command the Button";
+        }
+
+    }
+}
---
+++ Petzold.UriDialog/NavigateTheWeb.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Petzold.UriDialog
+{
+    class NavigateTheWeb : Window
+    {
+        Frame frm;
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Application app = new Application();
+            app.Run(new NavigateTheWeb());
+        }
+
+        public NavigateTheWeb()
+        {
+            Title = "Navigate the Web";
+            
+            frm = new Frame();
+            Content = frm;
+
+            Loaded += OnWindowLoaded;
+        }
+
+        void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            UriDialog dlg = new UriDialog();
+            dlg.Owner = this;
+            dlg.Text = "http://";
+            dlg.ShowDialog();
+
+            try
+            {
+                frm.Source = new Uri(dlg.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Title);
+            }
+
+        }
+    }
+}
