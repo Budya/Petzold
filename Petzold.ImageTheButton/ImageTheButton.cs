74600f6cc7a4c2ba1b1b064ac2edb50a8f37b09e
Budya <budinskiy_d@1mpf.lan>
2017-10-25 07:42:29 +0300

    25 morning

---
+++ Petzold.ImageTheButton/ImageTheButton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Petzold.ImageTheButton
+{
+    class ImageTheButton : Window
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Application app = new Application();
+            app.Run(new ImageTheButton());
+
+        }
+
+        public ImageTheButton()
+        {
+            Title = "Image The Button";
+            Uri uri = new Uri("pack://application:,,,/krik.png");
+            BitmapImage bitmap = new BitmapImage(uri);
+            Image img = new Image();
+            img.Source = bitmap;
+            img.Stretch = Stretch.None;
+
+            Button btn = new Button();
+            btn.Content = img;
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            btn.VerticalAlignment = VerticalAlignment.Center;
+
+            Content = btn;
+
+        }
+    }
+}
