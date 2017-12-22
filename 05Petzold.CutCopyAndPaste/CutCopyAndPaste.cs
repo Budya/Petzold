5c212914a15f3f945526781bbe6beef53a5b3785
Budya <budinskiy_d@1mpf.lan>
2017-12-22 17:02:15 +0300

    22/12/17

---
+++ 05Petzold.CutCopyAndPaste/CutCopyAndPaste.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Reflection;
+using FilePath = System.IO.Path;
+
+namespace _05Petzold.CutCopyAndPaste
+{
+    class CutCopyAndPaste : Window
+    {
+        private TextBlock text;
+        protected MenuItem itemCut, itemCopy, itemPaste, itemDelete;
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Application app = new Application();
+            app.Run(new CutCopyAndPaste());
+        }
+
+        public CutCopyAndPaste()
+        {
+            Title = "Cut, Copy, And Paste";
+            
+            var directory = FilePath.GetDirectoryName(
+                Assembly.GetExecutingAssembly().Location);
+
+            Console.WriteLine(directory.ToString());
+            
+
+            // Creation DockPanel
+            DockPanel dock = new DockPanel();
+            Content = dock;
+
+            // Creation menu, in top of wondow
+            Menu menu = new Menu();
+            dock.Children.Add(menu);
+            DockPanel.SetDock(menu, Dock.Top);
+
+            // Creation TextBlock for other free space
+            text = new TextBlock();
+            text.Text = "Sample clipboard text";
+            text.HorizontalAlignment = HorizontalAlignment.Center;
+            text.VerticalAlignment = VerticalAlignment.Center;
+            text.FontSize = 32;
+            text.TextWrapping = TextWrapping.Wrap;
+            dock.Children.Add(text);
+
+            // Creation menu Edit
+            MenuItem itemEdit = new MenuItem();
+            itemEdit.Header = "_Edit";
+            itemEdit.SubmenuOpened += EditOnOpened;
+            menu.Items.Add(itemEdit);
+
+            // Creation commands of Edit menu
+            itemCut = new MenuItem();
+            itemCut.Header = "Cu_t";
+            itemCut.Click += CutOnClick;
+            Image img = new Image();
+            img.Source = new BitmapImage(
+            new Uri("pack://application:,,/Images/cutIco.png"));
+            Console.WriteLine(img.Source.ToString());
+            itemCut.Icon = img;
+            itemCut.VerticalContentAlignment = VerticalAlignment.Center;
+            itemEdit.Items.Add(itemCut);
+
+            itemCopy = new MenuItem();
+            itemCopy.Header = "_Copy";
+            itemCopy.Click += CopyOnClick;
+            img = new Image();
+            img.Source = new BitmapImage(
+            new Uri("Images/copyIco.png", UriKind.Relative));
+            itemCopy.Icon = img;
+            itemEdit.Items.Add(itemCopy);
+
+            itemPaste = new MenuItem();
+            itemPaste.Header = "_Paste";
+            itemPaste.Click += PasteOnClick;
+            img = new Image();
+            img.Source = new BitmapImage(
+            new Uri("Images/pasteIco.png", UriKind.Relative));
+            itemPaste.Icon = img;
+            itemEdit.Items.Add(itemPaste);
+
+            itemDelete = new MenuItem();
+            itemDelete.Header = "_Delete";
+            itemDelete.Click += DeleteOnClick;
+            img = new Image();
+            img.Source = new BitmapImage(
+            new Uri("Images/deleteIco.png", UriKind.Relative));
+            itemDelete.Icon = img;
+            itemEdit.Items.Add(itemDelete);
+        }
+
+        private void EditOnOpened(object sender, RoutedEventArgs args)
+        {
+            itemCut.IsEnabled =
+            itemCopy.IsEnabled =
+            itemDelete.IsEnabled = text.Text != null && text.Text.Length > 0;
+            itemPaste.IsEnabled = Clipboard.ContainsText();
+        }
+
+        private void CutOnClick(object sender, RoutedEventArgs args)
+        {
+            CopyOnClick(sender, args);
+            DeleteOnClick(sender, args);
+        }
+        private void CopyOnClick(object sender, RoutedEventArgs e)
+        {
+            if (text.Text != null && text.Text.Length > 0)
+            Clipboard.SetText(text.Text);
+        }
+
+        private void PasteOnClick(object sender, RoutedEventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            text.Text = Clipboard.GetText();
+        }
+
+        private void DeleteOnClick(object sender, RoutedEventArgs e)
+        {
+            text.Text = null;
+        }
+    }
+}
